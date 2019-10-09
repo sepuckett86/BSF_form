7afb874eda339703335f3fc294e292616db0858c
sepuckett86 <sepuckett86@gmail.com>
2019-10-08 18:23:58 -0700

    add basic form and button for netlify

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import Form from './Form';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <Form />;
 }
-  
---
+++ src/components/Form.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import Button from './Button';
+import styles from './Form.css';
+
+export default function Form() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&');
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', name })
+    })
+      .then(() => alert('Success!'))
+      .catch(error => alert(error));
+  };
+
+  return (
+    <form className={styles.Form} onSubmit={handleSubmit}>
+      <p>
+        <label>
+          Name:
+          <input 
+            type="text" 
+            onChange={({ target }) => setName(target.value)}
+            value={name}
+          ></input>
+        </label>
+      </p>
+      <p>
+        <label>
+          Email:
+          <input 
+            type="text" 
+            onChange={({ target }) => setEmail(target.value)}
+            value={email}
+          ></input>
+        </label>
+      </p>
+      <p>
+        <label>
+          Message:
+          <input 
+            type="text" 
+            onChange={({ target }) => setMessage(target.value)}
+            value={message}
+          ></input>
+        </label>
+      </p>
+      <Button />
+    </form>
+  );
+}
